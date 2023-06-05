f58aee4ea90b68266af6c3e2c494c6f34b4700fb
setouchi-h <setouchi.kazuki@gmail.com>
2023-06-05 22:45:23 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+1. Relative Stability: Anchored or Pegged -> 1 coin = $1.00
+   1. Chainlink Price feed.
+   2. Set a function to exchange `1 ETH or BTC = $1.00`
+2. Stability Mechanism (Minting): Algorithmic (Decentralized)
+    1. People can only mint the stablecoin with enough collateral (coded)
+3. Collateral: Exogenous (Crypto)
+   1. wETH
+   2. wBTC
---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+
+
+/*
+ * @title DecentralizedStableCoin
+ * @author Kazuki
+ * Collateral: Exogenous (ETH & BTC)
+ * Minting: Algorithmic
+ * Relative Stability: Pegged to USD
+ *
+ * This is the contract meant to be governed by DSCEngine.
+ * This contract is just the ERC20 implementation of our stablecoin system.
+ */
+contract DecentralizedStableCoin {
+    constructor() {}
+}
