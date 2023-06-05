e7e8389898bcbbe022c368837f641e00da3ce416
setouchi-h <setouchi.kazuki@gmail.com>
2023-06-05 23:08:35 +0900

    openzeppelin

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -1,8 +1,31 @@
 // SPDX-License-Identifier: MIT
 
+// This is considered an Exogenous, Decentralized, Anchored (pegged), Crypto Collateralized low volitility coin
+
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
 pragma solidity ^0.8.18;
 
-
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 /*
  * @title DecentralizedStableCoin
@@ -14,6 +37,6 @@ pragma solidity ^0.8.18;
  * This is the contract meant to be governed by DSCEngine.
  * This contract is just the ERC20 implementation of our stablecoin system.
  */
-contract DecentralizedStableCoin {
-    constructor() {}
+contract DecentralizedStableCoin is ERC20Burnable {
+    constructor() ERC20("DecentralizedStableCoin", "DSC") {}
 }
